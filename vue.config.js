2175347e99c180c493abde46d514f3f36a25fa98
kostia7alania <osetia-alania@mail.ru>
2019-01-23 14:38:22 +0300

    относительный путь терь  должен тож пахать

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
+  baseUrl: process.env.NODE_ENV === 'production' ? './' : '',
   runtimeCompiler:true,
   configureWebpack: {
     devtool: 'source-map'
