35125d5ef450114c072ad8fa66a4fd18568ee0a2
Костя и Сергей <johndoe@example.com>
2019-01-28 17:18:53 +0300

    обновление NPM+постфикс к файлам вырубил

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
   baseUrl: process.env.NODE_ENV === 'production' ? './' : '',
   runtimeCompiler:true,
+  filenameHashing: false,
   configureWebpack: {
     devtool: 'source-map'
   }
