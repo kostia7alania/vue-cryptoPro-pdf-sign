177472b4cce1f4253b0c5ad8d5eae1b8796a39d1
103-servak <103@m.ru>
2019-04-25 11:17:48 +0300

    state to separate file

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,17 @@
-const state = {
-  production: 1, // 0 тест, 1 - продакшн 
-  backend_url: '',//'http://localhost:8383/rest-api',
-  saveState: 0,//как изначально работать с сохранением состояния (после обновления стр);
-  helpStatus: 'main', ////main,feedback,interactive
-  tour: false //включен ли режим тура; 
-};
-  
-  
+
+
 
 import Vue from 'vue';
 import Vuex from 'vuex'; Vue.use(Vuex);
 /*import Axios from 'axios';require('es6-promise').polyfill();var axios = require('axios');Vue.use(Axios);*/
- 
-import mutations from './mutations.js'; 
-import getters from './getters.js'; 
+
+import mutations from './mutations.js';
+import getters from './getters.js';
 import actions from './actions.js';
+import state from './state.js';
 
 window.store = new Vuex.Store({
-  state, 
+  state,
   getters,
   mutations,
   actions
