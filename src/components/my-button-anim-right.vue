353079a1bf177c60b1225719114e6a9252247881
kostia7alania <osetia-alania@mail.ru>
2019-07-10 20:32:25 +0300

    btn2

---
+++ src/components/my-button-anim-right.vue
@@ -0,0 +1,60 @@
+
+<template>
+  <button class="button" @click="$emit('click')" style="vertical-align:middle"><span>{{text}} </span></button>
+</template>
+
+<script>
+export default {
+  name: "my-button-anim",
+  props: {
+    text: {
+      default: () => "Click"
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.button {
+  display: inline-block;
+  border-radius: 4px;
+  background-color: #f4511e;
+  border: none;
+  color: #ffffff;
+  text-align: center;
+  font-size: 16px;
+  padding: 1 20px;
+  width: "100%";
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: "\00bb";
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+</style>
