070a173ab700c31b32d5df716d2c415a241a89d1
kostia7alania <osetia-alania@mail.ru>
2019-07-25 20:34:59 +0300

    add getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,10 +1,18 @@
- 
-const getters = { 
-  /*anychanges: s=> {   // return diff(s.copyObj,s.newJson) 
+
+const getters = {
+  /*anychanges: s=> {   // return diff(s.copyObj,s.newJson)
     return JSON.stringify(s.copyObj) != JSON.stringify(s.newJson) ///1-если есть изменения
-  },*/ 
+  },*/
   helpStatus: s => s.helpStatus,
-  tour: s =>0// s.tour
+  tour: s => 0,// s.tour
+
+  CERT_LIST_GETTER: state => state.CERT_LIST.length && state.CERT_LIST.map(e => {
+    return { text: e.name, value: e.id };
+  }),
+  DOC_ID: () => {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("id");
+  },
 }
 
 
